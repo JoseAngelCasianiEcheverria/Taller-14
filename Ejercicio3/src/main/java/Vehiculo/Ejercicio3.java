31bc76841f74df3e1422a19e914d5d44a8cfb52b
JoseAngelCasianiEcheverria <joseangelcasianiecheverria@gmail.com>
2025-03-11 21:20:26 -0500

    Ejercicio 3

---
+++ Ejercicio3/src/main/java/Vehiculo/Ejercicio3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Vehiculo;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
